4bc9af08edcc3bb00aa3499280cea1ab57733c77
Joose Helle <joose.helle@gmail.com>
2020-04-14 22:02:24 +0300

    Validate & debug graph conversion

--- src/common/igraph.py
+++ src/common/igraph.py
@@ -16,7 +16,7 @@ def get_edge_dicts(G: Graph, attrs: list = ['geometry']) -> list:
         edge_dicts.append(edge_dict)
     return edge_dicts
 
-def get_edge_gdf(G: Graph, id_attr: str = None, attrs: list = [], geom_attr: str = 'geometry', epsg: int = 4326) -> gpd.GeoDataFrame:
+def get_edge_gdf(G: Graph, id_attr: str = None, attrs: list = [], geom_attr: str = 'geometry', epsg: int = 3879) -> gpd.GeoDataFrame:
     """Returns edges of a graph as pandas GeoDataFrame. 
     """
     edge_dicts = []
@@ -29,6 +29,8 @@ def get_edge_gdf(G: Graph, id_attr: str = None, attrs: list = [], geom_attr: str
         for attr in attrs:
             if (attr in edge_attrs):
                 edge_dict[attr] = edge_attrs[attr]
+            elif (hasattr(edge, attr)):
+                edge_dict[attr] = getattr(edge, attr)
         edge_dicts.append(edge_dict)
 
     return gpd.GeoDataFrame(edge_dicts, index=ids, crs=from_epsg(epsg))
@@ -46,6 +48,8 @@ def get_node_gdf(G: Graph, id_attr: str = None, attrs: list = [], geom_attr: str
         for attr in attrs:
             if(attr in node_attrs):
                 node_dict[attr] = node_attrs[attr]
+            elif (hasattr(node, attr)):
+                node_dict[attr] = getattr(node, attr)
         node_dicts.append(node_dict)
 
     return gpd.GeoDataFrame(node_dicts, index=ids, crs=from_epsg(epsg))
--- src/otp2igraph_import/otp2igraph_import.py
+++ src/otp2igraph_import/otp2igraph_import.py
@@ -16,10 +16,11 @@ from common.logger import Logger
 
 log = Logger(printing=True, log_file='otp2igraph_import.log', level='info')
 hma_poly = geom_utils.project_geom(gpd.read_file('HMA.geojson')['geometry'][0])
-export_to_gpkg: bool = False
-debug_igraphs: bool = True
-otp_graph_gpkg = 'otp_graph_data/otp_graph_features.gpkg'
-graph_debug_gpkg = 'otp_graph_data/otp_igraph_debug.gpkg'
+export_otp_data_to_gpkg: bool = True
+export_decomposed_igraphs_to_gpkg: bool = True
+export_final_graph_to_gpkg: bool = True
+debug_otp_graph_gpkg = 'debug/otp_graph_features.gpkg'
+debug_igraph_gpkg = 'debug/otp2igraph_features.gpkg'
 
 # 1) read nodes nodes from CSV
 n = pd.read_csv('otp_graph_data/nodes.csv', sep=';')
@@ -50,12 +51,12 @@ e = e.to_crs(epsg=3879)
 log.debug(f'edges head: {e.head()}')
 
 # 3) export graph data go gpkg
-if (export_to_gpkg == True):
-    log.info('writing graph data to gpkg')
-    e.to_file(otp_graph_gpkg, layer='edges', driver='GPKG')
-    log.info(f'exported edges to {otp_graph_gpkg} (layer=edges)')
-    n.to_file(otp_graph_gpkg, layer='nodes', driver='GPKG')
-    log.info(f'exported nodes to {otp_graph_gpkg} (layer=nodes)')
+if (export_otp_data_to_gpkg == True):
+    log.info('writing otp graph data to gpkg')
+    e.drop(columns=['geom_wgs']).to_file(debug_otp_graph_gpkg, layer='edges', driver='GPKG')
+    log.info(f'exported edges to {debug_otp_graph_gpkg} (layer=edges)')
+    n.drop(columns=['geom_wgs']).to_file(debug_otp_graph_gpkg, layer='nodes', driver='GPKG')
+    log.info(f'exported nodes to {debug_otp_graph_gpkg} (layer=nodes)')
 
 # 4) filter out edges that are unsuitable for both walking and cycling
 def filter_df_by_query(df: pd.DataFrame, query: str, name: str = 'rows'):
@@ -82,7 +83,6 @@ log.info('adding nodes to graph')
 G = ig.Graph(directed=True)
 G.add_vertices(len(n.index))
 G.vs['id_otp'] = list(n['id_otp'])
-G.vs['id_ig'] = list(n['id_ig'])
 G.vs['geometry'] = list(n['geometry'])
 
 # 7) add edges to graph
@@ -100,14 +100,14 @@ G.es['uv_ig'] = list(e_filt['uv_ig'])
 G.es['geometry'] = list(e_filt['geometry'])
 # TODO add rest of the edge attributes
 
-# 8) delete graph features outside Helsinki Metropolitan Area (HMA)
+# 8) delete edges outside Helsinki Metropolitan Area (HMA)
 hma_buffered = hma_poly.buffer(100)
 
 def intersects_hma(geom: LineString):
     if (geom.is_empty == True): return True
     return True if geom.intersects(hma_buffered) else False
 
-e_gdf = ig_utils.get_edge_gdf(G, attrs=['id_otp', 'id_ig'])
+e_gdf = ig_utils.get_edge_gdf(G)
 log.info('finding edges that intersect with HMA')
 e_gdf['in_hma'] = [intersects_hma(line) for line in e_gdf['geometry']]
 e_gdf_del = e_gdf.query('in_hma == False').copy()
@@ -120,9 +120,15 @@ G.delete_edges(e_gdf_del.index.tolist())
 after_count = G.ecount()
 log.info(f'deleted {before_count-after_count} edges')
 
+# check if id_ig:s need to be updated to edge attributes
+mismatch_count = len([edge.index for edge in G.es if edge.attributes()['id_ig'] != edge.index])
+log.info(f'invalid edge ids: {mismatch_count}')
 # reassign igraph indexes to edge and node attributes
 G.es['id_ig'] = [e.index for e in G.es]
 G.vs['id_ig'] = [v.index for v in G.vs]
+# check if id_ig:s need to be updated to edge attributes
+mismatch_count = len([edge.index for edge in G.es if edge.attributes()['id_ig'] != edge.index])
+log.info(f'invalid edge ids: {mismatch_count} (after re-indexing)')
 
 # 9) find and inspect subgraphs by decomposing the graph
 sub_graphs = G.decompose(mode='STRONG')
@@ -160,20 +166,17 @@ for graph_id, graph in enumerate(big_graphs):
         edge['graph_id'] = graph_id
     big_graph_edges.extend(edges)
 
-if (debug_igraphs == True):
+if (export_decomposed_igraphs_to_gpkg == True):
     log.info('exporting subgraphs to gpkg')
     # graphs with <= 15 edges
-    small_graph_edges_gdf = gpd.GeoDataFrame(small_graph_edges, crs=from_epsg(4326))
-    log.info(f'{small_graph_edges_gdf.head()}')
-    small_graph_edges_gdf.to_file(graph_debug_gpkg, layer='small_graph_edges', driver='GPKG')
+    small_graph_edges_gdf = gpd.GeoDataFrame(small_graph_edges, crs=from_epsg(3879))
+    small_graph_edges_gdf.to_file(debug_igraph_gpkg, layer='small_graph_edges', driver='GPKG')
     # graphs with  15–500 edges
-    medium_graph_edges_gdf = gpd.GeoDataFrame(medium_graph_edges, crs=from_epsg(4326))
-    log.info(f'{medium_graph_edges_gdf.head()}')
-    medium_graph_edges_gdf.to_file(graph_debug_gpkg, layer='medium_graph_edges', driver='GPKG')
+    medium_graph_edges_gdf = gpd.GeoDataFrame(medium_graph_edges, crs=from_epsg(3879))
+    medium_graph_edges_gdf.to_file(debug_igraph_gpkg, layer='medium_graph_edges', driver='GPKG')
     # graphs with > 500 edges
-    big_graph_edges_gdf = gpd.GeoDataFrame(big_graph_edges, crs=from_epsg(4326))
-    log.info(f'{big_graph_edges_gdf.head()}')
-    big_graph_edges_gdf.to_file(graph_debug_gpkg, layer='big_graph_edges', driver='GPKG')
+    big_graph_edges_gdf = gpd.GeoDataFrame(big_graph_edges, crs=from_epsg(3879))
+    big_graph_edges_gdf.to_file(debug_igraph_gpkg, layer='big_graph_edges', driver='GPKG')
     log.info(f'graphs exported')
 
 # 10) delete smallest subgraphs from the graph
@@ -186,10 +189,31 @@ del_ratio = round(100 * (before_count-after_count) / before_count, 1)
 log.info(f'deleted {before_count-after_count} ({del_ratio} %) edges')
 
 # 11) delete isolated nodes from the graph
-del_node_ids = G.vs.select(_degree_eq=0)['id_ig']
+del_node_ids = [v.index for v in G.vs.select(_degree_eq=0)]
 log.info(f'deleting {len(del_node_ids)} isolated nodes')
 before_count = G.vcount()
 G.delete_vertices(del_node_ids)
 after_count = G.vcount()
 del_ratio = round(100 * (before_count-after_count) / before_count, 1)
 log.info(f'deleted {before_count-after_count} ({del_ratio} %) nodes')
+
+# check if id_ig:s need to be updated to edge attributes
+mismatch_count = len([edge.index for edge in G.es if edge.attributes()['id_ig'] != edge.index])
+log.info(f'invalid edge ids: {mismatch_count}')
+# reassign igraph indexes to edge and node attributes
+G.es['id_ig'] = [e.index for e in G.es]
+G.vs['id_ig'] = [v.index for v in G.vs]
+# check if id_ig:s need to be updated to edge attributes
+mismatch_count = len([edge.index for edge in G.es if edge.attributes()['id_ig'] != edge.index])
+log.info(f'invalid edge ids: {mismatch_count} (after re-indexing)')
+
+# 12) export graph data to GeoDataFrames fro debugging
+log.info(f'exporting final graph to {debug_igraph_gpkg} for debugging')
+e_gdf = ig_utils.get_edge_gdf(G, attrs=['id_otp', 'id_ig', 'source', 'target'])
+n_gdf = ig_utils.get_node_gdf(G, attrs=['index'])
+
+if (export_final_graph_to_gpkg == True):
+    e_gdf.to_file(debug_igraph_gpkg, layer='final_graph_edges', driver='GPKG')
+    n_gdf.to_file(debug_igraph_gpkg, layer='final_graph_nodes', driver='GPKG')
+
+log.info('all done')
