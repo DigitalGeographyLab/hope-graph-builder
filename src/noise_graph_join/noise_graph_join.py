cff6a10e9a6b6ba1dd8b2db7f026b26abcf08634
Joose Helle <joose.helle@gmail.com>
2020-05-21 11:25:55 +0300

    Adjust sampling in nodata areas

--- src/noise_graph_join/noise_graph_join.py
+++ src/noise_graph_join/noise_graph_join.py
@@ -21,7 +21,15 @@ def noise_graph_join(
 
     graph = ig_utils.read_graphml(graph_file)
     log.info(f'read graph of {graph.ecount()} edges')
+
+    # read nodata zone: narrow area between noise surfaces of different municipalities
     nodata_zone = gpd.read_file(nodata_zone_gpkg_layer['gpkg'], layer=nodata_zone_gpkg_layer['layer'])
+
+    # read noise data
+    noise_layer_names = [layer for layer in fiona.listlayers(noise_gpkg)]
+    noise_layers = { name: gpd.read_file(noise_gpkg, layer=name) for name in noise_layer_names }
+    noise_layers = { name: gdf.rename(columns={'db_low':name}) for name, gdf in noise_layers.items() }
+    log.info(f'read {len(noise_layers)} noise layers')
 
     # create sampling points
     gdf = ig_utils.get_edge_gdf(graph, attrs=[E.id_ig])
@@ -43,12 +51,9 @@ def noise_graph_join(
         uniq_point_gdf.to_file(debug_gpkg, layer='sampling_points', driver='GPKG')
 
     # spatially join noise values by sampling points from a set of noise surface layers
-    noise_layers = [layer for layer in fiona.listlayers(noise_gpkg)]
     point_noises = uniq_point_gdf.copy()
-    for layer in noise_layers:
-        log.debug(f'joining noise layer [{layer}] to sampling points')
-        noise_gdf = gpd.read_file(noise_gpkg, layer=layer)
-        noise_gdf = noise_gdf.rename(columns={'db_low':layer})
+    for name, noise_gdf in noise_layers.items():
+        log.debug(f'joining noise layer [{name}] to sampling points')
         point_noises = gpd.sjoin(point_noises, noise_gdf, how='left', op='within').drop(['index_right'], axis=1)
     
     point_noises['no_noise_values'] = point_noises.apply(lambda row: utils.all_noise_values_none(row, noise_layers), axis=1)
@@ -63,7 +68,7 @@ def noise_graph_join(
 
     # create extra sampling points for sampling points missing noise values
     missing_noises = point_noises[point_noises['missing_noises'] == True][['xy_id', 'geometry']].copy()
-    missing_noises['sampling_points'] = [utils.get_sampling_points_around(point, distance=12, count=20) for point in missing_noises['geometry']]
+    missing_noises['sampling_points'] = [utils.get_sampling_points_around(point, distance=7, count=20) for point in missing_noises['geometry']]
     extra_sampling_points = utils.explode_extra_sampling_point_gdf(missing_noises)
 
     if (b_debug == True):
@@ -71,10 +76,8 @@ def noise_graph_join(
 
     # join noise values to extra sampling points
     extra_sampling_point_noises = extra_sampling_points.copy()
-    for layer in noise_layers:
-        log.debug(f'joining noise layer [{layer}] to additional sampling points')
-        noise_gdf = gpd.read_file(noise_gpkg, layer=layer)
-        noise_gdf = noise_gdf.rename(columns={'db_low':layer})
+    for name, noise_gdf in noise_layers.items():
+        log.debug(f'joining noise layer [{name}] to additional sampling points')
         extra_sampling_point_noises = gpd.sjoin(extra_sampling_point_noises, noise_gdf, how='left', op='within').drop(['index_right'], axis=1)
     
     if (b_debug == True):
@@ -83,11 +86,11 @@ def noise_graph_join(
     # calculate average noise values per xy_id from extra sampling points
     extra_samples_by_xy_id = extra_sampling_point_noises.groupby(by='xy_id')
     extra_noise_samples = []
-    for name, group in extra_samples_by_xy_id:
+    for xy_id, group in extra_samples_by_xy_id:
         samples = group.copy()
         samples = samples.fillna(0)
-        extra_sample = {layer: samples[layer].median() for layer in noise_layers}
-        extra_sample['xy_id'] = name
+        extra_sample = {name: samples[name].quantile(.8, interpolation='nearest') for name in noise_layers.keys()}
+        extra_sample['xy_id'] = xy_id
         extra_noise_samples.append(extra_sample)
 
     extra_noise_samples_df = pd.DataFrame(extra_noise_samples)
