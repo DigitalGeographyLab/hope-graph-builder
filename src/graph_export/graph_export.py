8e17ced8c39bd0629dc4da4960ea21b152415363
Joose Helle <joose.helle@gmail.com>
2020-06-07 12:56:49 +0300

    Add revised AQI fetcher & updater

--- src/graph_export/graph_export.py
+++ src/graph_export/graph_export.py
@@ -6,7 +6,7 @@ from common.schema import Edge as E, Node as N
 graph = ig_utils.read_graphml('data/kumpula_noises.graphml')
 
 out_node_attrs = [N.geometry]
-out_edge_attrs = [E.uv, E.geometry, E.geom_wgs, E.length, E.length_b, E.noises, E.noise_source]
+out_edge_attrs = [E.id_ig, E.uv, E.geometry, E.geom_wgs, E.length, E.length_b, E.noises, E.noise_source]
 
 def set_biking_length(graph, edge_gdf):
     for edge in edge_gdf.itertuples():
