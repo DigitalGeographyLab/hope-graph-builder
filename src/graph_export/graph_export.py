7e11779ed142b632e8e673b039f5350e88295466
Joose Helle <joose.helle@gmail.com>
2020-06-23 10:46:56 +0300

    Add edge attribute for identifying similar geometries

--- src/graph_export/graph_export.py
+++ src/graph_export/graph_export.py
@@ -7,7 +7,7 @@ graph = ig_utils.read_graphml('data/hma.graphml')
 out_graph = 'out_graph/hma.graphml'
 
 out_node_attrs = [N.geometry]
-out_edge_attrs = [E.id_ig, E.uv, E.geometry, E.geom_wgs, E.length, E.length_b, E.noises, E.noise_source]
+out_edge_attrs = [E.id_ig, E.uv, E.id_way, E.geometry, E.geom_wgs, E.length, E.length_b, E.noises, E.noise_source]
 
 def set_biking_lengths(graph, edge_gdf):
     # TODO use walking speed when allows_biking=False and/or traversable_biking=False ?
@@ -19,14 +19,20 @@ def set_biking_lengths(graph, edge_gdf):
         graph.es[getattr(edge, E.id_ig.name)][E.length_b.value] = round(biking_length, 4)
 
 def set_uv(graph, edge_gdf):
-    for edge in edge_gdf.itertuples():
-        source = getattr(edge, 'source')
-        target = getattr(edge, 'target')
-        graph.es[getattr(edge, E.id_ig.name)][E.uv.value] = (source, target)
+    edge_gdf['uv'] = edge_gdf.apply(lambda x: (x['source'], x['target']), axis=1)
+    graph.es[E.uv.value] = list(edge_gdf['uv'])
+
+def set_way_ids(graph, edge_gdf):
+    edge_gdf['way_id'] = edge_gdf.apply(lambda x: str(round(x['length'], 1))+str(sorted(x['uv'])), axis=1)
+    way_ids = list(edge_gdf['way_id'].unique())
+    way_ids_d = { way_id: idx for idx, way_id in enumerate(way_ids) }
+    edge_gdf['way_id'] = [way_ids_d[way_id] for way_id in edge_gdf['way_id']]
+    graph.es[E.id_way.value] = list(edge_gdf['way_id'])
 
 edge_gdf = ig_utils.get_edge_gdf(graph, attrs=[E.id_ig, E.length, E.bike_safety_factor], ig_attrs=['source', 'target'])
 
 set_biking_lengths(graph, edge_gdf)
 set_uv(graph, edge_gdf)
+set_way_ids(graph, edge_gdf)
 
 ig_utils.export_to_graphml(graph, out_graph, n_attrs=out_node_attrs, e_attrs=out_edge_attrs)
